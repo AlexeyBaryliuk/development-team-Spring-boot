b683b274fd28b0885ea484a111815840e1823b2e
AlexeyBaryliuk <alexeybaryliuk@gmail.com>
2020-05-31 14:21:40 +0300

    Update

--- development-team-web-app/src/test/java/com/epam/brest/courses/web_app/HelloControllerIT.java
+++ development-team-web-app/src/test/java/com/epam/brest/courses/web_app/HelloControllerIT.java
@@ -1,14 +1,13 @@
 package com.epam.brest.courses.web_app;
 
-import com.epam.brest.courses.web_app.testConfig.TestConfig;
 import com.epam.brest.courses.web_app.config.viewConfig.ViewConfig;
+import com.epam.brest.courses.web_app.testConfig.TestConfig;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -19,7 +18,6 @@ import org.springframework.web.context.WebApplicationContext;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(classes={TestConfig.class, ViewConfig.class} )
-@ActiveProfiles("test")
 class HelloControllerIT {
 
     @Autowired
